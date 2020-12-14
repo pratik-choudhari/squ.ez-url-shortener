eb4a332a0e19a013300e42cf4bbe445796faa390
pratik-choudhari <pratik.choudhari89@gmail.com>
2020-12-14 22:17:03 +0530

    API with fixed bugs

---
+++ database.py
@@ -0,0 +1,52 @@
+import sqlite3
+import random
+import string
+import re
+
+conn = sqlite3.connect("url.db", check_same_thread=False)
+
+def check_if_exists(id):
+    query = f'''SELECT COUNT(*) FROM URLS WHERE ID="{id}"'''
+    db_res = conn.execute(query)
+    if [i[0] for i in db_res] == [0]:
+        return False
+    else:
+        return True
+
+def insert_data(id, og, value):
+    query = f'''INSERT INTO URLS (ID, ORIGINAL, VISITS) VALUES ("{str(id)}", "{str(og)}", {int(value)});'''
+    db_res = conn.execute(query)
+    conn.commit()
+    if not db_res:
+        return False
+    else:
+        return True
+
+def get_original_url(id):
+    query = f'''SELECT ORIGINAL FROM URLS WHERE ID="{str(id)}";'''
+    db_res = conn.execute(query)
+    url = [i[0] for i in db_res]
+    if url:
+        return url[0]
+    else:
+        return False
+
+def get_valid_combination(url: str)-> str:
+    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"""
+    res = re.findall(regex, url)
+    url = url.replace("https://www", "")
+    url = url.replace("http://www", "")
+    url = url.replace("https://", "")
+    url = url.replace("http://", "")
+    print(url)
+    data = False
+    if res:
+        while 1:
+            shrt = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+            if not check_if_exists(shrt):
+                if not insert_data(shrt, url, 0):
+                    return False
+                else:
+                    data = "".join(["localhost/", shrt])
+                    break
+    return data
--- main.py
+++ main.py
@@ -1,52 +1,53 @@
-from flask import Flask, jsonify,render_template, request
+from flask import Flask, jsonify,render_template, request, redirect
 import warnings
 import re
 import json
 import random, string
-import sqlite3
+from database import check_if_exists, insert_data, get_original_url, get_valid_combination
 warnings.filterwarnings('ignore')
 random.seed(10)
 
 # app
 app = Flask(__name__)
-conn = sqlite3.connect("url.db")
 
-# generate url
-def get_valid_combination(url: str)-> str:
-    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"""
-    res = re.findall(regex, url)
-    data = "Invalid URL"
-    if res:
-        flag = True
-        while flag:
-            shrt = ''.join(random.choice(string.ascii_letters) for _ in range(8))
-            query = f'''SELECT COUNT(ID) FROM URLS WHERE ID={shrt}'''
-            db_res = conn.execute(query)
-            if [i[0] for i in db_res] == [0]:
-                query = f'''INSERT INTO URLS (ID, ORIGINAL, VISITS) VALUES ({shrt}, {url}, 0);'''
-                db_res = conn.execute(query)
-                if not db_res:
-                    return "Error inserting data"
-                data = "".join(["localhost/", shrt])
-                flag = False
-                break
-    return data
 
 # routes
-@app.route('/', methods=['GET','POST'])
-def homepage():
-    return render_template('index.html')
 
-@app.route('/shorten', methods=['POST'])
+
+@app.route('/shorten', methods=['GET', 'POST'])
 def results():
     url = request.args.get("url")
-    data = get_valid_combination(url)
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    if url:
+        data = get_valid_combination(url)
+        if data:
+            response = app.response_class(
+                response=json.dumps(data),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
+        else:
+            return render_template("invalid.html")
+    else:
+        return render_template("index.html")
+
+
+@app.route('/<cmpt_url>', methods=['GET', 'POST'])
+def redirect_logic(cmpt_url):
+    if not cmpt_url:
+        return render_template('index.html')
+    else:
+        url = get_original_url(cmpt_url)
+        if url:
+            url = "".join(["http://www.", url])
+            return redirect(url, code=302)
+        else:
+            return render_template("invalid.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port = 5000, debug=True)
